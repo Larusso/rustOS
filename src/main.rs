fdb3c4d57997db31e9c6c1c5fac8907f37d7cc9f
Manfred Endres <manfred.endres@tslarusso.de>
2018-11-24 14:48:37 +0100

    Step three: compile bootable bootloader image

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
+static HELLO: &[u8] = b"Welcome to RustOS!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
