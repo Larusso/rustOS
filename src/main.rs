341c1f6e578c555ae9cdf7cf3f005807cf7d3006
Manfred Endres <manfred.endres@tslarusso.de>
2018-11-24 14:12:07 +0100

    Step one: a freestanding binary

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn mainCRTStartup() -> ! {
+    main();
+}
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    loop {}
+}
